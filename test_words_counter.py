fe066aa7f56bfa9f06f9fcf7ac03e83703518691
Yakuza <cedrik1410@gmail.com>
2022-09-28 21:34:01 +0200

    Adding counter option

--- test_words_counter.py
+++ test_words_counter.py
@@ -1,11 +1,11 @@
 import pytest
-from words_counter import words_counter
+from words_counter import text_splitter, words_counter
+
+def test_text_splitter():
+    assert text_splitter("Some example text, given here.") == ['Some', 'example', 'text', 'given', 'here']
 
 
 def test_words_counter():
-    assert words_counter("Lorem ipsum dolor sit amet.") in {"lorem": 1,
-                                                            "ipsum": 1,
-                                                            "dolor": 1,
-                                                            "sit": 1,
-                                                            "amet": 1
-                                                            }
+    list_of_words = ['Some', 'some', 'exaple', 'teXt', 'given', 'Here', 'just', 'for', 'fun']
+    assert words_counter(list_of_words) == {'some': 2, 'exaple': 1, 'text': 1, 'given': 1, 'here': 1, 'just': 1,
+                                            'for': 1, 'fun': 1}
--- words_counter.py
+++ words_counter.py
@@ -1,16 +1,34 @@
+import re
+def text_splitter(text):
+    list_of_words = re.split(r'\W+', text)
+    if '' in list_of_words:
+        list_of_words.remove('')
+    return list_of_words
 
-def words_counter(text):
+def words_counter(words_list):
     counted_words = {}
-    # ToDo find the way to get read of the dots and commas
-    splited_text = text.split()
-    words_list = [word.lower() for word in splited_text]
 
     for word in words_list:
-        if word in counted_words.keys():
-            counted_words[word] = counted_words[word] + 1
+        if word.lower() in counted_words.keys():
+            counted_words[word.lower()] = counted_words[word.lower()] + 1
         else:
-            counted_words[word] = 1
+            counted_words[word.lower()] = 1
 
     return counted_words
 
-print(words_counter("This this, is is a test test file, For for my ProGram"))
+text_1 = "Lorem ipsum dolor sit amet."
+text_2 = "Test, test, kolejny, dla potrzeb, tej funkcji."
+text_3 = "Some some, exaple teXt, given. Here, just, for fun"
+
+list_1 = text_splitter(text_1)
+list_2 = text_splitter(text_2)
+list_3 = text_splitter(text_3)
+print(list_3)
+
+words_1 = words_counter(list_1)
+words_2 = words_counter(list_2)
+words_3 = words_counter(list_3)
+
+print(words_1)
+print(words_2)
+print(words_3)
