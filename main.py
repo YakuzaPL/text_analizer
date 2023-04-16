b0038347bda6fc695f1cac37529fda98472612f4
Yakuza <cedrik1410@gmail.com>
2023-04-16 19:38:18 +0200

    Fixined some solutions

--- main.py
+++ main.py
@@ -7,14 +7,41 @@ import os
 
 
 dir_list = os.listdir('files/')
-
-
-menu_list = ['Choose file to analise.', 'Print the number of letters in text', 'Print the number of words in text',
+chose_file_menu = ['Choose file to analise.', 'EXIT']
+menu_list = ['Print the number of letters in text', 'Print the number of words in text',
              'Save the results in CSV file', 'EXIT']
+chosen_file = None
 
 is_on = True
 while is_on:
 
+
+    while chosen_file == None:
+        for index, option in enumerate(chose_file_menu):
+            print(f'{index + 1} - {option}')
+        first_menu_pick = int(input('Choose your option: '))
+
+        if chose_file_menu[first_menu_pick - 1] == 'Choose file to analise.':
+            file_list = []
+            # for direction in dir_list:
+            #     file_list.append(direction.split('.')[0])
+            for index, name in enumerate(dir_list):
+                print(f'{index + 1}-{name}')
+            user_file_name = int(input("Choose a file number to proceed: "))
+            chosen_file = dir_list[user_file_name - 1]
+
+            file_name = chosen_file.split('.')[0]
+            file_extention = chosen_file.split('.')[-1]
+
+            if file_extention == 'docx':
+                file_receiv.open_docx_file()
+            text = open_txt_file(file_name)
+
+        elif menu_list[first_menu_pick -1] == 'EXIT':
+            print('Thank you for useing our software! Good bye!')
+            is_on = False
+
+    print(f'"{file_name}" file is chosen!')
     for index, option in enumerate(menu_list):
         print(f'{index + 1 } - {option}')
 
@@ -22,19 +49,6 @@ while is_on:
     if menu_list[user_choice-1] == 'EXIT':
         print('Thank you for useing our software! Good bye!')
         is_on = False
-    elif menu_list[user_choice-1] == 'Choose file to analise.':
-        file_list = []
-        chosen_file = None
-        for direction in dir_list:
-            file_list.append(direction.split('.')[0])
-        for index, name in enumerate(file_list):
-            print(f'{index + 1}-{name}')
-        user_file_name = int(input("Choose a file number to proceed: "))
-        chosen_file = file_list[user_file_name - 1]
-        if f'{chosen_file}.docx' in dir_list:
-            file_receiv.open_docx_file()
-        text = open_txt_file(chosen_file)
-        menu_list[0] = f'"{chosen_file}" file is chosen!'
     elif menu_list[user_choice-1] == 'Print the number of letters in text':
         letter_list = text_to_letters(text)
         letter_count_dict = words_or_letters_dict_creator(letter_list)
@@ -54,8 +68,8 @@ while is_on:
             print('Thank you!')
             is_on = False
     elif menu_list[user_choice - 1] == 'Save the results in CSV file':
-        creating_CSV.creator_csv(word_count_dict,f'{chosen_file}_words')
-        creating_CSV.creator_csv(letter_count_dict, f'{chosen_file}_letters')
+        creating_CSV.creator_csv(word_count_dict,f'{file_name}_words')
+        creating_CSV.creator_csv(letter_count_dict, f'{file_name}_letters')
         continue_chooice = input('Continue? [Y/N]')
         if continue_chooice.lower() == 'n':
             print('Thank you!')
